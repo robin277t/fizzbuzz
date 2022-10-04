d9d51c083d5f41b803c3fcfb3e757853b8ced077
Tim Buller <111061411+tbuller@users.noreply.github.com>
2022-10-04 12:22:34 +0200

    Round 1 final

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,15 @@
-def fizzbuzz(num)
-    if num == 5 
-        return "buzz"
+def fizzbuzz
+  for num in 1..100 do
+    if num % 3 == 0 && num % 5 != 0
+      puts "fizz"
+    elsif num % 5 == 0 && num % 3 != 0
+      puts "buzz"
+    elsif num % 3 == 0 && num % 5 == 0
+      puts "fizzbuzz"
+    else
+      puts num
     end
+  end  
 end
 
-fizzbuzz(5)
+fizzbuzz
--- fizzbuzz_spec.rb
+++ fizzbuzz_spec.rb
@@ -1,7 +1,19 @@
 require './fizzbuzz'
 describe 'fizzbuzz' do
-    it 'number' do
+    it 'buzz' do
         expect(fizzbuzz(5)).to eq 'buzz'
     end
-end
+    
+    it 'fizz' do
+        expect(fizzbuzz(3)).to eq 'fizz'
+    end
+    
+    it 'num' do
+        expect(fizzbuzz(4)).to eq 4
+    end
+
+    it "fizzbuzz" do
+        expect(fizzbuzz(15)).to eq "fizzbuzz"
+    end    
+end        
 
